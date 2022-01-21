c64b3355d00d1dfc30e4d6c4c12d2eb913199bb5
duceppemo <17859096+duceppemo@users.noreply.github.com>
2022-01-21 14:56:57 -0500

    Fixed extract Now fully working

--- extract_SNP_regions.py
+++ extract_SNP_regions.py
@@ -1,9 +1,8 @@
 #!/usr/local/env python3
 
-import os
 import gzip
 from argparse import ArgumentParser
-from collections import defaultdict
+from itertools import groupby
 
 
 __author__ = 'duceppemo'
@@ -16,16 +15,17 @@ class Ref_Extractor(object):
         self.ref = args.reference
         self.vcf = args.variant
         self.bp = args.base_pairs
-        self.output = args.output
+        self.output = args.output_fasta
 
         # Run
         vcf_dict = Methods.parse_vcf(self.vcf)
-        Methods.extract_regions(self.ref, vcf_dict, self.bp, self.output)
+        Methods.extract_regions_iter(self.ref, vcf_dict, self.bp, self.output)
 
 
 class Methods(object):
     @staticmethod
     def parse_vcf(vcf_file):
+        print('Parsing VCF file...')
         vcf_dict = dict()
         with gzip.open(vcf_file, 'rt') if vcf_file.endswith('.gz') else open(vcf_file, 'r') as f:
             for line in f:
@@ -40,36 +40,40 @@ class Methods(object):
                 if chrom not in vcf_dict:
                     vcf_dict[chrom] = [pos]
                 else:
-                    vcf_dict[chrom].append(pos)
+                    if pos not in vcf_dict[chrom]:  # Avoid duplicated positions
+                        vcf_dict[chrom].append(pos)
 
         return vcf_dict
 
     @staticmethod
-    def extract_regions(ref, vcf_dict, bp, out):
+    def extract_regions_iter(ref, vcf_dict, bp, out):
+        # https://www.biostars.org/p/710/
         with open(out, 'w') as fh_out:
-            ref_dict = dict()
+            print('Parsing reference fasta file...')
             with gzip.open(ref, 'rt') if ref.endswith('.gz') else open(ref, 'r') as fh_in:
-                for line in fh_in:
-                    line = line.rstrip()
-                    if not line:
-                        continue
-                    if line.startswith('>'):
-                        chrom = line.split()[0].replace('>', '')
-                        ref_dict[chrom] = ''
-                    else:
-                        ref_dict[chrom] += line
-            for chrom, pos in vcf_dict.items():
-                index = int(pos) - 1
-                start = index - int(bp)
-                if start <= 0:
-                    start = 0
-                stop = index + int(bp)
-                if stop > len(ref_dict[chrom]):
-                    stop = len(ref_dict[chrom])
+                # Create iterator
+                faiter = (x[1] for x in groupby(fh_in, lambda line: line[0] == '>'))
 
-                header = '>{}:{}-{}'.format(chrom, start, stop)
-                seq = ref_dict[chrom][start:stop]
-                fh_out.write('{}\n{}\n'.format(header, seq))
+                for header in faiter:
+                    # drop the ">"
+                    chrom = header.__next__()[1:].rstrip().split()[0]
+                    # join all sequence lines to one.
+                    full_seq = ''.join(s.rstrip() for s in faiter.__next__())
+                    if chrom in vcf_dict:
+                        for pos in vcf_dict[chrom]:
+                            index = int(pos) - 1  # position 1 is index 0
+                            start = index - int(bp)
+                            if start <= 0:  # if the SNP is closer to beginning of sequence than the range required
+                                start = 0
+                            stop = index + int(bp)
+                            if stop > len(full_seq):  # if the SNP is closer to end of sequence than the range required
+                                stop = len(full_seq)
+
+                            header = '>{}:{}-{} SNP at {}'.format(chrom, start + 1, stop + 1, index + 1)  # index -> position
+                            extracted_region = full_seq[start:stop + 1]
+                            fh_out.write('{}\n{}\n'.format(header, extracted_region))
+
+                    # yield tuple(header, seq)
 
 
 if __name__ == "__main__":
